cb80239ccbb9e17acd220e9223de52abc950b375
RaySherb <ras0032@alumni.uah.edu>
2021-03-29 22:37:18 -0500

    edits

--- main_stroke.R
+++ main_stroke.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(caret)
 library(scales)
-library(psych)
+
 # Load the stroke data
 healthcare_data <- read_csv('healthcare-dataset-stroke-data.csv')
 # Extract Validation set
@@ -22,17 +22,27 @@ stroke <- stroke %>% mutate(smoking_status=fct_relevel(as.factor(smoking_status)
                                                        'formerly smoked',
                                                        'never smoked',
                                                        'Unknown'))
+# Other variables as factors will help models later on
+stroke <- stroke %>% mutate(gender=as.factor(gender),
+                            hypertension=as.factor(hypertension),
+                            heart_disease=as.factor(heart_disease),
+                            ever_married=as.factor(ever_married),
+                            work_type=as.factor(work_type),
+                            Residence_type=as.factor(Residence_type),
+                            stroke=as.factor(stroke),
+                            bmi=as.numeric(bmi)) # bmi has NA's
+
 
 # Barplot of stroke & gender
 stroke %>% group_by(stroke, gender) %>% summarise(n=n()) %>%
-  ggplot(aes(x=as.factor(stroke), fill=gender, y=n))+
+  ggplot(aes(x=stroke, fill=gender, y=n))+
   geom_col(position='dodge', color='black')+
   ylab('Count')+
   xlab('Stroke')+
   scale_fill_brewer(palette = "Pastel1")
 # The dataset has a lot more females, and about equal amount of strokes
 stroke %>% group_by(stroke, gender) %>% summarise(n=n()) %>%
-  ggplot(aes(x=gender, fill=as.factor(stroke), y=n))+
+  ggplot(aes(x=gender, fill=stroke, y=n))+
   geom_col(position='fill', color='black')+
   scale_y_continuous(label=scales::percent)+
   xlab('Stroke')+
@@ -49,13 +59,13 @@ stroke[stroke$stroke == 1, ] %>% group_by(gender) %>%
 # Women have a steep increase later in life
 
 stroke %>% group_by(stroke) %>%
-  ggplot(aes(x=avg_glucose_level, color=as.factor(stroke)))+
+  ggplot(aes(x=avg_glucose_level, color=stroke))+
   geom_histogram(bins=30)
 # Bimodal distribution. high glucose has higher risk of stroke
 
 # Smoking Status
 stroke %>% group_by(smoking_status, stroke) %>% summarise(n=n()) %>%
-  ggplot(aes(x=as.factor(stroke), y=n, fill=smoking_status))+
+  ggplot(aes(x=stroke, y=n, fill=smoking_status))+
   geom_col(position='fill', color='black')+
   scale_y_continuous(label=scales::percent)
 # Seems a lot of smokers never get a stroke
@@ -69,8 +79,8 @@ stroke[stroke$stroke == 1, ] %>% group_by(smoking_status) %>%
 
 # Hypertension (high blood pressure), heart disease, & high bmi
 stroke %>% group_by(stroke, hypertension) %>% summarise(n=n()) %>%
-  ggplot(aes(x=hypertension, y=n, color=as.factor(stroke)))+
-    geom_col(position='fill', color='black')
+  ggplot(aes(x=hypertension, y=n, color=stroke))+
+    geom_col(position='fill')
 
 
 #############################################################
@@ -82,23 +92,36 @@ stroke %>% group_by(stroke, hypertension) %>% summarise(n=n()) %>%
 # 
 #############################################################
 
-# Remove the solution vector
-#y <- stroke$stroke
-#stroke <- stroke %>% select(-stroke)
+
+stroke <- stroke %>% select(-bmi)
 # Get a test/train set of data
 set.seed(69, sample.kind = 'Rounding')
-test_index <- createDataPartition(y, times = 1, p = 0.1, list = FALSE)
+test_index <- createDataPartition(stroke$stroke, times = 1, p = 0.1, list = FALSE)
 test <- stroke[test_index, ]
 train <- stroke[-test_index, ]
 rm(test_index)
 
 # Baseline will be predicting the 'healthy' for entire population
+# Sensitivity = 0
+# Specificity = 1
+mean(train$stroke == 0)
 
 # Tree method
-train_rpart <- train(stroke ~ .,
+train_tree <- train(stroke ~ .,
                      method='rpart',
                      data=train,
                      tuneGrid=data.frame(cp=seq(0, 0.05, len=25)))
-ggplot(train_rpart, highlight=T)
-plot(train_rpart$finalModel, margin=0.1)
-text(train_rpart$finalModel, cex=0.75)
+ggplot(train_tree, highlight=T)
+plot(train_tree$finalModel, margin = 0.1)
+text(train_tree$finalModel)
+y_hat_tree <- predict(train_tree, test)
+confusionMatrix(y_hat_tree, test$stroke)$overall["Accuracy"]
+
+# Random Forest
+train_forest <- train(stroke ~ .,
+                      method='rf',
+                      nodeSize=1,
+                      tuneGrid = data.frame(mtry = seq(50, 200, 25)),
+                      data=train)
+ggplot(train_forest)
+confusionMatrix(predict(train_forest, test), test$stroke)
